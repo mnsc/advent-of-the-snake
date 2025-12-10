fefa14ea94083dde2556d70f269883cef28c7c97
Mattias Blom <mattias.blom@sartorius.com>
2025-12-10 21:03:15 +0100

    day 3 part2

--- 2025/src/day3.py
+++ 2025/src/day3.py
@@ -1,19 +1,30 @@
-def get_joltage(bank: str):
-    max_val = 0
-    for x in range(0, len(bank) - 1):
-        for y in range(x + 1, len(bank)):
-            max_val = max(max_val, int(bank[x] + bank[y]))
-    return max_val
+def get_joltage(bank: str, no_of_flips: int):
+    length = len(bank)
+    start = 0
+    joltage_digits = []
+    for flip in range(no_of_flips):
+        this_max = 0
+        for chk in range(start, length - no_of_flips + flip + 1):
+            chk_digit = int(bank[chk])
+            if chk_digit > this_max:  # strict gt
+                this_max = chk_digit
+                start = chk + 1
+        joltage_digits.append(this_max)
+    return int(''.join(map(str, joltage_digits)))
+
+
+def sum_up_joltage(lines: list[str], part, flips):
+    total_j = 0
+    for bank in lines:
+        total_j += get_joltage(bank, flips)
+    print(f"Part {part} - total joltage = {total_j}")
 
 
 def main():
     with open("day3.txt") as f:
         lines = f.read().splitlines()
-        total_j = 0
-        for bank in lines:
-            total_j += get_joltage(bank)
-
-        print(f"part 1 - total joltage = {total_j}")
+        sum_up_joltage(lines, 1, 2)
+        sum_up_joltage(lines, 2, 12)
 
 
 if __name__ == '__main__':
--- 2025/tests/test_day3.py
+++ 2025/tests/test_day3.py
@@ -6,10 +6,17 @@ from day3 import get_joltage
 class TestDay3(unittest.TestCase):
 
     def test_short(self):
-        self.assertEqual(23, get_joltage("123"))
+        self.assertEqual(23, get_joltage("123", 2))
 
     def test_mid(self):
-        self.assertEqual(54, get_joltage("225341"))
+        self.assertEqual(54, get_joltage("225341", 2))
 
     def test_long(self):
-        self.assertEqual(87, get_joltage("65422318111112224766666644555"))
+        self.assertEqual(87, get_joltage("65422318111112224766666644555", 2))
+
+    def test_three_flips(self):
+        self.assertEqual(887, get_joltage("8855555557", 3))
+
+
+    def test_twelve_flips(self):
+        self.assertEqual(987654321111, get_joltage("987654321111111", 12))
